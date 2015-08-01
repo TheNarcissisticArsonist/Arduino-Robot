60ce2f64e3025f0fe33be5579cc1d8201f787499
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-08-01 18:38:25 -0400

    Basic program structure.

---
+++ Robot/Robot.ino
@@ -0,0 +1,14 @@
+#include <SPI.h>
+#include <WiFi.h>
+
+char ssid[] = "secretNetworkName";
+char pass[] = "secretPassword";
+int status = WL_IDLE_STATUS;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
