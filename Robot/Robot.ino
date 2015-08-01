ddaf81d20aa518bac8c5a7c59feda23e6b726671
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-08-01 19:35:17 -0400

    Since it's not a web browser, I can do this differently.

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -26,44 +26,35 @@ void loop() {
   WiFiClient client = server.available();
   if(client) {
     String cont = "";
-    boolean done = false;
-    while(client.available() && !done) {
+    while(client.available()) {
       char c = client.read();
       Serial.write(c);
       cont += c;
-      if(cont == "KEY DOWN w") {
-        Serial.write("w");
-        done = true;
-      }
-      if(cont == "KEY DOWN a") {
-        Serial.write("a");
-        done = true;
-      }
-      if(cont == "KEY DOWN s") {
-        Serial.write("s");
-        done = true;
-      }
-      if(cont == "KEY DOWN d") {
-        Serial.write("d");
-        done = true;
-      }
+    }
+    if(cont == "KEY DOWN w") {
+      Serial.write("w");
+    }
+    if(cont == "KEY DOWN a") {
+      Serial.write("a");
+    }
+    if(cont == "KEY DOWN s") {
+      Serial.write("s");
+    }
+    if(cont == "KEY DOWN d") {
+      Serial.write("d");
+    }
 
-      if(cont == "KEY UP w") {
-        Serial.write("!w");
-        done = true;
-      }
-      if(cont == "KEY UP a") {
-        Serial.write("!a");
-        done = true;
-      }
-      if(cont == "KEY UP s") {
-        Serial.write("!s");
-        done = true;
-      }
-      if(cont == "KEY UP d") {
-        Serial.write("!d");
-        done = true;
-      }
+    if(cont == "KEY UP w") {
+      Serial.write("!w");
+    }
+    if(cont == "KEY UP a") {
+      Serial.write("!a");
+    }
+    if(cont == "KEY UP s") {
+      Serial.write("!s");
+    }
+    if(cont == "KEY UP d") {
+      Serial.write("!d");
     }
   }
 }
