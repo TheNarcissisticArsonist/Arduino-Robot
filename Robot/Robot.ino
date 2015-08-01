0f3066ad1d60f3a179acf7d22631f02cabf02806
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-08-01 18:40:25 -0400

    Start serial, connect to WiFi.

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -5,8 +5,15 @@ char ssid[] = "secretNetworkName";
 char pass[] = "secretPassword";
 int status = WL_IDLE_STATUS;
 
-void setup() {
+WiFiServer server(80);
 
+void setup() {
+  Serial.begin(9600);
+  status = WiFi.begin(ssid, pass);
+  if(status != WL_CONNECTED) {
+    Serial.println("WiFi not connected!");
+    while(true);
+  }
 }
 
 void loop() {
