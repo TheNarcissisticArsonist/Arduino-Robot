a726f7383d7eae734a185bc143e3979d5854d38a
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-08-01 20:29:59 -0400

    Set up as Java project.

---
+++ src/Client.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+public class Client {
+	public static void main(String[] args) {
+		
+	}
+}
