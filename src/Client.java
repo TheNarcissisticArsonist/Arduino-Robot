6312f17a76e0fc4264ff0453518708d5ddcc528b
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-08-01 20:31:38 -0400

    Create a window, make it say hello, and close it when you press space.

--- src/Client.java
+++ src/Client.java
@@ -7,6 +7,20 @@ import java.awt.event.KeyEvent;
 
 public class Client {
 	public static void main(String[] args) {
-		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setContentPane(new JPanel(new BorderLayout()));
+		frame.getContentPane().add(new JLabel("Hi!"), BorderLayout.CENTER);
+		frame.setVisible(true);
+		frame.addKeyListener(new KeyListener() {
+			public void keyTyped(KeyEvent e) {}
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+					System.exit(0);
+				}
+			}
+			public void keyReleased(KeyEvent e) {}
+		});
 	}
 }
